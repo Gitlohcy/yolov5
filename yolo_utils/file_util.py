46379858e9c2b3857a98755e2575b39f38cb0801
lohcy <loh.cheeyung96@gmail.com>
2021-01-26 15:30:11 +0800

    update write labels functions

--- yolo_utils/file_util.py
+++ yolo_utils/file_util.py
@@ -1,5 +1,9 @@
 from .general import *
 
+
+def mkdir_r
+
+#read files
 def yoloLabel_lines_2_list(img, yolo_lines):
     ih, iw , _ = img.shape
     yolo_lines = np.array(yolo_lines).astype('float')
@@ -23,3 +27,33 @@ def lbl_from_img_name(lbl_dest, img_fname):
     fname = Path(img_fname).stem
     lbl_path = lbl_dest/(fname + '.txt')
     return lbl_path
+
+
+# write labels
+def uniq_file_name(prefix=None):
+    hex_name = uuid.uuid4().hex
+    uniq_name = prefix + '_' + hex_name if prefix else  hex_name
+    return uniq_name
+
+def list_2_yoloLabel_lines(img, yolo_bboxes):
+    ih, iw , _ = img.shape
+    yolo_bboxes = np.array(yolo_bboxes).astype('float')
+    
+    yolo_bboxes[:, 1:5]  = (yolo_bboxes[:, 1:5] / [iw, ih, iw, ih]).round(4)
+    str_lines = yolo_bboxes.astype('str')
+    return str_lines
+
+def f_writelines(lines: List[str], fname, join_by=' '):
+    lines = [join_by.join(list(line)) +'\n' for line in lines]
+    
+    with open(str(fname), 'w') as f:
+        f.writelines(lines)
+
+def write_img_and_bboxes(img, labels, img_dest, lbl_dest):
+    fname = uniq_file_name('img')
+    img_name = fname + '.jpg'
+    lbl_name = fname + '.txt'
+    
+    imageio.imwrite(img_dest/img_name, img)
+    yolo_lines = list_2_yoloLabel_lines(img, labels)
+    f_writelines(yolo_lines, lbl_dest/lbl_name)
